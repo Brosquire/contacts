91844a5c1f2350cdb91f59a8e170da08933cad4a
Brosquire <zhf@ZHFs-iMac.local>
2020-05-01 11:34:44 -0700

    added User model and register/login routes with JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 //  @Route  GET api/auth
 //  @Desc   get logged in user
@@ -11,8 +17,53 @@ router.get("/", (req, res) => {
 //  @Route  POST api/auth
 //  @Desc   Authenticate user and get token
 //  @Access Public
-router.post("/", (req, res) => {
-  res.send("Log In User");
-});
+router.post(
+  "/",
+  [
+    check("email", "Valid email is required").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ msg: "Invalid Credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000, // change back to 3600 for production
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
 
 module.exports = router;
